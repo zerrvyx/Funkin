1477766d54f3ccaa62b7b3b4e251389d555c4ed4
l1ttleO <l1ttleofficial@outlook.com>
2021-12-09 17:00:59 +0500

    Release v2.0-rc.3

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -24,7 +24,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var projectFnfVersion:String = '2.0 (RC 2)';
+	public static var projectFnfVersion:String = '2.0 (RC 3)';
 	public static var psychEngineVersion:String = '0.4.2'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
